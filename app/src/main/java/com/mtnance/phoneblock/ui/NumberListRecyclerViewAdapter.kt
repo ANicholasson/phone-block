6c6b2d98621f2b9e45e0d11a1de99bec94f41741
Aron Nicholasson <aronmar@live.no>
2021-08-26 21:05:31 +0200

    Added recycler view template/placeholder

---
+++ app/src/main/java/com/mtnance/phoneblock/ui/NumberListRecyclerViewAdapter.kt
@@ -0,0 +1,48 @@
+package com.mtnance.phoneblock.ui
+
+import androidx.recyclerview.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.TextView
+
+import com.mtnance.phoneblock.placeholder.PlaceholderContent.PlaceholderItem
+import com.mtnance.phoneblock.databinding.FragmentNumberItemBinding
+
+/**
+ * [RecyclerView.Adapter] that can display a [PlaceholderItem].
+ * TODO: Replace the implementation with code for your data type.
+ */
+class NumberListRecyclerViewAdapter(
+    private val values: List<PlaceholderItem>
+) : RecyclerView.Adapter<NumberListRecyclerViewAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+
+        return ViewHolder(
+            FragmentNumberItemBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = values[position]
+        holder.idView.text = item.id
+        holder.contentView.text = item.content
+    }
+
+    override fun getItemCount(): Int = values.size
+
+    inner class ViewHolder(binding: FragmentNumberItemBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        val idView: TextView = binding.itemNumber
+        val contentView: TextView = binding.content
+
+        override fun toString(): String {
+            return super.toString() + " '" + contentView.text + "'"
+        }
+    }
+
+}
